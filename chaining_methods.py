9089ae15e2f2ad98632692f1f79e07f8e100db12
jschuster4 <jspowerex@gmail.com>
2022-05-03 12:55:17 -0500

    updated solution

--- chaining_methods.py
+++ chaining_methods.py
@@ -6,12 +6,16 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return(self)
 
     def make_withdrawl(self, amount):
         self.account_balance -= amount
+        return(self)
 
     def display_balance(self):
         print(self.account_balance)
+        return(self)
+        # why do I need a return self?
 
     def transfer_money(self, amount):
         self.make_withdrawl(amount)
@@ -26,22 +30,10 @@ Joel = User("joel" , "joel@codingdojo.com")
 Matt = User("Matt" , "matt@codingdojo.com") 
 Ashley = User("Ashley" , "ashley@codingdojo.com")
 
-Joel.make_deposit(100)
-Joel.make_deposit(200)
-Joel.make_deposit(300)
-Joel.make_withdrawl(400)
-Joel.display_balance()
+Joel.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawl(400).display_balance()
 
-Matt.make_deposit(500)
-Matt.make_deposit(300)
-Matt.make_withdrawl(600)
-Matt.make_withdrawl(100)
-Matt.display_balance()
+Matt.make_deposit(500).make_deposit(300).make_withdrawl(600).make_withdrawl(100).display_balance()
 
-Ashley.make_deposit(1000)
-Ashley.make_withdrawl(200)
-Ashley.make_withdrawl(100)
-Ashley.make_withdrawl(260)
-Ashley.display_balance()
+Ashley.make_deposit(1000).make_withdrawl(200).make_withdrawl(100).make_withdrawl(260).display_balance()
 
 Joel.transfer_money(100)
