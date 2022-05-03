cc319d4a9e75846fd13e9af5dc2aaa1f995e9578
jschuster4 <jspowerex@gmail.com>
2022-05-03 12:44:37 -0500

    User OOP practice

---
+++ User.py
@@ -0,0 +1,52 @@
+from mimetypes import init
+from string import ascii_letters
+from sys import displayhook
+
+
+class User: 
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account_balance = 0
+
+    def make_deposit(self, amount):
+        self.account_balance += amount
+
+    def make_withdrawl(self, amount):
+        self.account_balance -= amount
+
+    def display_balance(self):
+        print(self.account_balance)
+
+    def transfer_money(self, amount):
+        self.make_withdrawl(amount)
+        print("Name:", self.name, "balance", self.account_balance)
+        Ashley.make_deposit(amount)
+        print("Name:", Ashley.name, "balance:", Ashley.account_balance)
+        
+
+
+
+Joel = User("joel" , "joel@codingdojo.com")
+Matt = User("Matt" , "matt@codingdojo.com") 
+Ashley = User("Ashley" , "ashley@codingdojo.com")
+
+Joel.make_deposit(100)
+Joel.make_deposit(200)
+Joel.make_deposit(300)
+Joel.make_withdrawl(400)
+Joel.display_balance()
+
+Matt.make_deposit(500)
+Matt.make_deposit(300)
+Matt.make_withdrawl(600)
+Matt.make_withdrawl(100)
+Matt.display_balance()
+
+Ashley.make_deposit(1000)
+Ashley.make_withdrawl(200)
+Ashley.make_withdrawl(100)
+Ashley.make_withdrawl(260)
+Ashley.display_balance()
+
+Joel.transfer_money(100)
